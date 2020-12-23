f2966e5f49a461fa85e51b44f6a27c0254afca77
taebum choi <taebumchoi@taebumui-MacBook-Pro.local>
2020-12-23 14:56:25 +0900

    v0.1

---
+++ js/function.js
@@ -0,0 +1,27 @@
+$(function() {
+  $(".left.pane").resizable({
+    handles: "e, w",
+    minWidth:154,
+    maxWidth:300
+  });
+  $(".right.pane").resizable({
+    minWidth:305,
+    maxWidth:600,
+    handles: "e, w",
+    resize: function(event, ui) {
+      ui.position.left = 0;
+    }
+  });
+
+/*
+  $('.project').hover(function(){
+      $('.add_project').removeClass('hidden');
+  }, function() {
+      $('.add_project').addClass('hidden');
+  }
+  );
+*/
+
+
+
+});
